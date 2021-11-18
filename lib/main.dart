b9275c528da9a38b9c41fc7c4ffb5dec7b56c465
LAB TI <info@unira.ac.id>
2021-11-18 12:07:26 +0700

    Part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,6 +7,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const labelStyle = TextStyle(
+      fontFamily: 'JuliusSansOne',
+      color: Colors.white,
+      fontSize: 18
+    );
+    const textStyle = TextStyle(
+      fontFamily: 'Pacifico',
+      color: Colors.white,
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -37,21 +47,180 @@ class MyApp extends StatelessWidget {
                 ),
               )
             ),
-            Center(
-              child: RichText(
-                text: const TextSpan(
-                  children: <TextSpan>[
-                    TextSpan(text: 'The', style: TextStyle(fontFamily: 'Pacifico', fontSize:40)),
-                    TextSpan(text: '\nLOVE\n', style: TextStyle(fontFamily: 'Poppins', fontSize:70, fontWeight: FontWeight.w900, color: Colors.white, height: 0.8)),
-                    TextSpan(text: 'Calculator', style: TextStyle(fontFamily: 'Pacifico', fontSize:40, height: 1)),
-                  ]
-                )
-              )
-            )
+            FormPerson(labelStyle: labelStyle, textStyle: textStyle)
           ]
         ),
         ),
       ),
+    );
+  }
+}
+
+class FormPerson extends StatefulWidget {
+  const FormPerson({
+    Key key,
+    @required this.labelStyle,
+    @required this.textStyle,
+  }) : super(key: key);
+
+  final TextStyle labelStyle;
+  final TextStyle textStyle;
+
+  @override
+  _FormPersonState createState() => _FormPersonState();
+}
+
+class _FormPersonState extends State<FormPerson> {
+  final _formKey = GlobalKey<FormState>();
+  final _person1Controller = TextEditingController();
+  final _person2Controller = TextEditingController();
+
+
+  void _calculateLove() {
+    // sembunyikan keypad
+    SystemChannels.textInput.invokeMethod('TextInput.hide');
+    final full = (_person1Controller.text + _person2Controller.text).toLowerCase();
+    var total = 0;
+    for (var i = 0; i < full.length; i++) {
+      total += full.codeUnitAt(i);
+    }
+    final result = total % 101;
+    final snackBar = SnackBar(content: Text('Hasilnya Adalah: $result'));
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    _person1Controller.dispose();
+    _person2Controller.dispose();
+  }
+  @override
+  void initState(){
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      child: Column(
+        children: <Widget>[
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: 65, vertical: 0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(padding: const EdgeInsets.all(6.0),
+                  child: Text('Person 1', style: widget.labelStyle,)
+                  ), 
+                  InputPerson(labelStyle: widget.labelStyle, index: 1, controller: _person1Controller,),
+                  Padding(padding: const EdgeInsets.only(top: 17, bottom: 15),
+                  child: Text('Loves', style: widget.textStyle.copyWith(fontSize: 22))
+                  ),
+                  Padding(padding: const EdgeInsets.all(6.0),
+                  child: Text('Person 2', style: widget.labelStyle,)
+                  ),
+                  InputPerson(labelStyle: widget.labelStyle, index: 2, controller: _person2Controller,),
+                ],
+              ),
+            )
+          )),
+          Padding(
+              padding: EdgeInsets.only(bottom: 75),
+              child: GestureDetector(
+                onTap: () {
+                  if (_formKey.currentState.validate()) {
+                    _calculateLove();
+                  } 
+                },
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 65, vertical: 0),
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Color(0xffbb1f50), Color(0xffe95d83)
+                      ]
+                    ),
+                    border: Border.all(color: Colors.black26),
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [ BoxShadow(
+                      color: Colors.white.withAlpha(60),
+                      blurRadius: 7,
+                      spreadRadius: 2
+                    )]
+                  ),
+                  child: Center(
+                    child: Text('Calculate', style: widget.textStyle.copyWith(fontSize: 20))
+                  ),
+                ),
+              ),
+            )
+        ]
+      )
+    );
+  }
+}
+
+class InputPerson extends StatefulWidget {
+  const InputPerson({
+    Key key,
+    @required this.labelStyle, this.index,
+    @required this.controller
+  }) : super(key: key);
+
+  final TextStyle labelStyle;
+  final int index;
+  final TextEditingController controller;
+
+  @override
+  _InputPersonState createState() => _InputPersonState();
+}
+
+class _InputPersonState extends State<InputPerson> {
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(decoration: BoxDecoration(
+      borderRadius: BorderRadiusDirectional.circular(30),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.transparent,
+          blurRadius: 7,
+          spreadRadius: 1
+        )
+      ]
+    ),
+    child: TextFormField (
+      controller: widget.controller,
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Person ${widget.controller} must be not empty!';
+        }
+        return null;
+      },
+      autocorrect: false,
+      textCapitalization: TextCapitalization.none,
+      style: widget.labelStyle.copyWith(fontSize:16, color: Color(0xff55021e), fontWeight: FontWeight.bold),
+      decoration: InputDecoration(
+        filled: true,
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 20, vertical: 13 ),
+          hintText: 'Person ${widget.index}',
+          fillColor: Color(0xffdea8d0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(color: Colors.transparent)
+          )                             
+        ),
+      )
     );
   }
 }
